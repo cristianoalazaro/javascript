37a1572b87ca1a68b12b31735b0cd399c37204d3
Cristiano Ap Lázaro <cristianoalazaro@hotmail.com>
2020-12-02 16:01:16 -0300

    agenda2

--- node/projetoagenda/src/controllers/loginController.js
+++ node/projetoagenda/src/controllers/loginController.js
@@ -1,11 +1,26 @@
 const Login = require('../models/LoginModel');
-const { index } = require("./homeController");
 
 exports.index = (req,res)=>{
     res.render('login');
 };
 
-exports.register = (req,res)=>{
-    const login = new Login(req.body);
-    res.send(login.body);
+exports.register = async (req,res)=>{
+    try{
+        const login = new Login(req.body);
+        await login.register();
+        if(login.errors.length > 0){
+            req.flash('errors',login.errors);
+            req.session.save(function(){
+                return res.redirect('/login');
+            });
+            return;
+        }
+        req.flash('success','Seu usuário foi criado com sucesso!');
+        req.session.save(function(){
+            return res.redirect('/login');
+        })
+    }catch(e){
+        console.log(e);
+        return res.render('404');
+    }
 };
