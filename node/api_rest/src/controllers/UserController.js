4a8326c6d6531d455052731207e4549d900fe9a5
Cristiano Ap Lázaro <cristianoalazaro@hotmail.com>
2020-12-05 19:42:29 -0300

    seedUsers

---
+++ node/api_rest/src/controllers/UserController.js
@@ -0,0 +1,76 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    try {
+      const novoUser = await User.create(req.body);
+      const {id, nome, email} = novoUser;
+      return res.json({id, nome, email});
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map(error=>error.message)
+      });
+    }
+  };
+
+  async index(req, res){
+    try{
+      const users = await User.findAll({attributes:['id','nome','email']});
+      return res.json(users);
+    }catch(e){
+      return res.json(null);
+    }
+  };
+
+  async show(req, res){
+    try{
+      const user = await User.findByPk(req.params.id);
+
+      const {id, nome, email} = user;
+      return res.json({id, nome, email});
+    }catch(e){
+      return res.json(null);
+    }
+  };
+
+  async update(req, res){
+    try{
+      const user = await User.findByPk(req.userId);
+
+      if(!user){
+        return res.status(400).json({
+          errors:['Usuário não existe'],
+        });
+      }
+      const usuarioAlterado = await user.update(req.body);
+      const {id, nome, email} = usuarioAlterado;
+      return res.json({id, nome, email});
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map(error=>error.message),
+      });
+    }
+  };
+
+  async delete(req, res){
+    try{
+
+      const user = await User.findByPk(req.userId);
+
+      if(!user){
+        return res.status(400).json({
+          errors:['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(`usuário ${req.userId} deletado com sucesso!`)
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map(error=>error.message),
+      });
+    }
+  };
+};
+
+export default new UserController();
---
+++ node/api_rest/src/routes/userRoutes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import userController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
+
+const router = new Router();
+
+//Não everiam existir essas 2 rotas abaixo
+router.get('/', userController.index); //lista todos os usuários
+//router.get('/:id', userController.show); // lista um usuário
+
+router.post('/', userController.store);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired, userController.delete);
+
+export default router;
+
+/*
+  FUNÇÕES PADRÃO DO CRUD NO CONTROLLER
+  index -> lista todos os usuários -> GET
+  store/create -> cria um novo usuário -> POST
+  delete -> apaga um usuário -> DELETE
+  show -> mostra um usuário -> GET
+  update -> atualiza um usuário -> PUT/PATCH
+*/
